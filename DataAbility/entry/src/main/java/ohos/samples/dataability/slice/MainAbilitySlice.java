bda45871867fe4554c91966261ba521e1ca0f3c0
yl <839452019@qq.com>
2021-10-03 17:34:59 +0800

    aaaaa

--- DataAbility/entry/src/main/java/ohos/samples/dataability/slice/MainAbilitySlice.java
+++ DataAbility/entry/src/main/java/ohos/samples/dataability/slice/MainAbilitySlice.java
@@ -49,7 +49,7 @@ import java.util.ArrayList;
  * MainAbilitySlice
  */
 public class MainAbilitySlice extends AbilitySlice {
-    private String TAG8 = "MainAbilitySlice333333338" ;
+    private String TAGa = "Maaaaaa" ;
     private static final String TAG = MainAbilitySlice.class.getSimpleName();
 
     private static final HiLogLabel LABEL_LOG = new HiLogLabel(3, 0xD000F00, TAG);
